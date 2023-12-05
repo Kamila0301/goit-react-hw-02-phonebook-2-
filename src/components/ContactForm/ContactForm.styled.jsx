101c3aafa85168f31428783303e682e7c45abb5d
Kamila0301 <bogda0301@gmail.com>
2023-12-05 17:12:02 +0200

    update

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Form, ErrorMessage } from 'formik';
+
+export const StyledForm = styled(Form)`
+  background-color: black;
+`;
+export const StyledError = styled(ErrorMessage)`
+  color: red;
+`;
